df98ca15484a565a9e1cdda8c9e7aed1dc5db4f7
gizmo <niquimiqui@gmail.com>
2026-04-19 22:39:54 -0500

    Bug Solved!

--- Assets/Scripts/Mechanics/DeadZone.cs
+++ Assets/Scripts/Mechanics/DeadZone.cs
@@ -9,10 +9,7 @@ public class DeathZone : MonoBehaviour
         PlayerHealth ph = other.GetComponent<PlayerHealth>();
         if (ph == null) return;
 
-        if (!ph.IsInvincible)
-        {
-            ph.StartInvincibility();           // parpadeo mientras reaparece
-            GameManager.Instance.FallDeath();  // quita 1 vida + respawn
-        }
+        ph.StartInvincibility();           // parpadeo mientras reaparece
+        GameManager.Instance.FallDeath();  // quita 1 vida + respawn
     }
 }
